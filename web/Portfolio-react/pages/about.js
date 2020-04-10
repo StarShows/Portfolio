996c5e4fc2356c5bf837edb55638a8cabc23d0f4
Alex Tannnebaum <AlexTannenbaum@Admins-MBP-4.home>
2020-04-10 00:57:57 -0400

    Made a starwars theme

--- web/Portfolio-react/pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../comps/CoolLayout";
-
-export default function About() {
-  return (
-    <Layout>
-      <p>This is the about page</p>
-    </Layout>
-  );
-}
--- web/Portfolio-react/pages/portfolio.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../comps/CoolLayout";
-
-export default function Portfolio() {
-  return (
-    <Layout>
-      <p>This is the portfolio area</p>
-    </Layout>
-  );
-}
