cbb0c88129454a4ac2fc2ad0b4c42906e3e94d2b
Alex Tannnebaum <AlexTannenbaum@Admins-MBP-4.home>
2020-04-11 17:49:31 -0400

    Upgraded the star method to be wrapped in its own container

--- web/js/starBackground.js
+++ web/js/starBackground.js
@@ -1,21 +1,33 @@
-// Sets the number of stars we wish to display
 const numStars = 100;
 
-// For every star we want to display
-for (let i = 0; i < numStars; i++) {
-  let star = document.createElement("div");  
-  star.className = "star";
-  var xy = getRandomPosition();
-  star.style.top = xy[0] + 'px';
-  star.style.left = xy[1] + 'px';
-  document.body.append(star);
-}
+/* ======== */
 
-// Gets random x, y values based on the size of the container
-function getRandomPosition() {  
-  var y = window.outerWidth;
-  var x = window.outerHeight;
-  var randomX = Math.floor(Math.random()*x);
-  var randomY = Math.floor(Math.random()*y);
-  return [randomX,randomY];
-}
+// Sets the number of stars we wish to display
+(generateStars = (numberOfStars) => {
+
+  // Create a sky for the stars.
+  const wrapper = document.createElement('section');
+  wrapper.className = "sky";
+  document.body.append(wrapper);
+
+  // Set star coordinates
+  getRandomPosition = () => {
+    var y = window.outerWidth;
+    var x = window.outerHeight;
+    var randomX = Math.floor(Math.random() * x);
+    var randomY = Math.floor(Math.random() * y);
+
+    return [randomX, randomY];
+  }
+
+  // fill the sky with stars
+  for (let i = 0; i < numberOfStars; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    var xy = this.getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    wrapper.append(star);
+  }
+})(numStars);
+
